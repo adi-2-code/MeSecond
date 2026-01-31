7cc2efe727e1e4dbadef66e341a08873c649e0fb
Adi <adinarayanp2020@gmail.com>
2026-01-31 15:48:53 +0530

    singly linked list.c
    Basic data structure concept linked list.

---
+++ singly linked list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Insert at beginning
+struct Node* insert(struct Node* head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = head;
+    return newNode;
+}
+
+// Display list
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    head = insert(head, 10);
+    head = insert(head, 20);
+    head = insert(head, 30);
+
+    display(head);
+
+    return 0;
+}
