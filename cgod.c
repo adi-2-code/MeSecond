8d5220c7d1d3369c451ecb3efd45723cb071ac6a
Adi <adinarayanp2020@gmail.com>
2026-01-31 15:35:36 +0530

    twosum.c

--- cgod.c
+++ cgod.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-void main()
-{
-	printf("Hello World!");
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = (int*)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
 }
